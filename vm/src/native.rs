cc91a6055449da5f008b4d43be8a16d0a6b40ded
jekky <11986158+jac3km4@users.noreply.github.com>
2021-07-11 01:23:02 +0100

    Extract metadata to value

--- vm/src/native.rs
+++ vm/src/native.rs
@@ -28,129 +28,131 @@ pub fn default_pool() -> ConstantPool {
 
 #[rustfmt::skip]
 pub fn register_natives(vm: &mut VM, on_log: impl Fn(String) + 'static) {
-    vm.metadata_mut().register_native(
+    let meta = vm.metadata_mut();
+
+    meta.register_native(
         "OperatorAdd;StringString;String", 
         |x: String, y: String| Ret(x + &y)
     );
 
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorAdd;Int32Int32;Int32",
         |x: i32, y: i32| Ret(x + y)
     );
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorAssignAdd;OutInt32Int32;Int32",
         |x: i32, y: i32| RetOut(x + y, x + y)
     );
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorSubtract;Int32Int32;Int32",
         |x: i32, y: i32| Ret(x - y)
     );
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorAssignSubtract;OutInt32Int32;Int32",
         |x: i32, y: i32| RetOut(x - y, x - y)
     );
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorMultiply;Int32Int32;Int32",
         |x: i32, y: i32| Ret(x * y)
     );
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorAssignMultiply;OutInt32Int32;Int32",
         |x: i32, y: i32| RetOut(x * y, x * y)
     );
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorDivide;Int32Int32;Int32",
         |x: i32, y: i32| Ret(x / y)
     );
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorAssignDivide;OutInt32Int32;Int32",
         |x: i32, y: i32| RetOut(x / y, x / y)
     );
 
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorEqual;Int32Int32;Bool",
         |x: i32, y: i32| Ret(x == y)
     );
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorLess;Int32Int32;Bool",
         |x: i32, y: i32| Ret(x < y)
     );
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorLessEqual;Int32Int32;Bool",
         |x: i32, y: i32| Ret(x <= y)
     );
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorGreater;Int32Int32;Bool",
         |x: i32, y: i32| Ret(x > y)
     );
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorGreaterEqual;Int32Int32;Bool",
         |x: i32, y: i32| Ret(x >= y)
     );
 
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorAdd;FloatFloat;Float",
         |x: f32, y: f32| Ret(x + y)
     );
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorAssignAdd;OutFloatFloat;Float",
         |x: f32, y: f32| RetOut(x + y, x + y)
     );
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorSubtract;FloatFloat;Float",
         |x: f32, y: f32| Ret(x - y)
     );
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorAssignSubtract;OutFloatFloat;Float",
         |x: f32, y: f32| RetOut(x - y, x - y)
     );
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorMultiply;FloatFloat;Float",
         |x: f32, y: f32| Ret(x * y)
     );
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorAssignMultiply;OutFloatFloat;Float",
         |x: f32, y: f32| RetOut(x * y, x * y)
     );
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorDivide;FloatFloat;Float",
         |x: f32, y: f32| Ret(x / y)
     );
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorAssignDivide;OutFloatFloat;Float",
         |x: f32, y: f32| RetOut(x / y, x / y)
     );
 
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorEqual;FloatFloat;Bool",
         |x: f32, y: f32| Ret(x == y)
     );
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorLess;FloatFloat;Bool",
         |x: f32, y: f32| Ret(x < y)
     );
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorLessEqual;FloatFloat;Bool",
         |x: f32, y: f32| Ret(x <= y)
     );
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorGreater;FloatFloat;Bool",
         |x: f32, y: f32| Ret(x > y)
     );
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorGreaterEqual;FloatFloat;Bool",
         |x: f32, y: f32| Ret(x >= y)
     );
 
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorLogicAnd;BoolBool;Bool",
         |x: bool, y: bool| Ret(x && y)
     );
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "OperatorLogicOr;BoolBool;Bool",
         |x: bool, y: bool| Ret(x || y)
     );
 
-    vm.metadata_mut().register_native(
+    meta.register_native(
         "Log",
         on_log
     );
